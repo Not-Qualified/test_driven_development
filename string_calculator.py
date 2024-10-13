4b93e83ac5969b4651bb87950b7d2737ea72a873
timetrix <dharmesh.srk@gmail.com>
2024-10-13 12:47:29 +0530

    Multiple numbers tests added

--- string_calculator.py
+++ string_calculator.py
@@ -4,4 +4,7 @@ class StringCalculator:
         # If not numbers, return 0
         if not numbers:
             return 0
-        
+
+        # Returns the sum of numbers
+        nums = [int(num) for num in numbers.split(",")]
+        return sum(nums)
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -10,6 +10,14 @@ class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(self.calculator.add(""), 0)
 
+    def test_single_number(self):
+        self.assertEqual(self.calculator.add("1"), 1)
+    
+    def test_two_numbers(self):
+        self.assertEqual(self.calculator.add("1,4"), 5)
+    
+    def test_multiple_numbers(self):
+        self.assertEqual(self.calculator.add("1,2,3,4,5"), 15)
 
 
 if __name__ == "__main__":
